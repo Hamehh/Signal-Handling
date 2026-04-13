c8de8b1bcec59ddcdb31d17281dd46167dc8bc5f
Hamehh <hamedkhan401@gmail.com>
2026-04-13 15:51:20 -0500

    Update signal handler output format

--- SIGINT.cpp
+++ SIGINT.cpp
@@ -7,7 +7,7 @@ bool running = true;
 
 void signal_handler(int signum)
 {
-    printf("Caught signal %d\n", signum);
+    printf("\nCaught signal %d\n", signum);
     fflush(stdout);
     running = false;
 }
